a91d23a090260299e593b380c3bb6bde36c5580e
Charles Anesu Kombo <62628677+Charl070@users.noreply.github.com>
2024-05-11 19:50:50 +0200

    project setup

---
+++ src/containers/otpScreen/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+
+
+const OtpScreen = () => {
+    
+    return (
+        <View>
+            <Text>
+                  This is OTP Screen
+            </Text>
+        </View>
+    )
+
+}
+
+export default OtpScreen
